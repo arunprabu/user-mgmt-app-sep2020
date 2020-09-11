8853eb9734b73f10698c20dff0f9d76462892ab5
Arun Vijayarengan <arunprabu.v@hotmail.com>
2020-09-11 11:14:03 +0530

    day 4

--- src/app/users/components/user-details/user-details.component.ts
+++ src/app/users/components/user-details/user-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-details',
@@ -11,11 +12,14 @@ export class UserDetailsComponent implements OnInit {
 
   contactData: any;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     // ajax calls
-    this.userService.getUserByUserId(1)
+    // read url param in angular
+    const userId = this.activatedRoute.snapshot.paramMap.get('userId');
+
+    this.userService.getUserByUserId(userId)
       .subscribe( (res: any) => {
         console.log(res);
         this.contactData = res;
